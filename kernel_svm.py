85bd209c130118a2027c9d427e8681820b1b4559
Franklin Qin <franklinqin0@gmail.com>
2022-07-29 08:35:54 +0200

    alt attack on kernel svm?

--- kernel_svm.py
+++ kernel_svm.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 
 learning_rate = 1e-1
 num_epochs = 100
-N = 300
+N = 10
 TEST_SIZE = 100
 TRAIN_SIZE = 30
 
@@ -59,18 +59,43 @@ def fgsm(model, x, y, epsilon):
     # print('sign', x.grad.data.sign())
     return epsilon * x.grad.data.sign()
 
-def fit(num_epochs, x_train, y_train, x_test, y_test, model, opt, epsilon):
+def pgd_l2(model, x, y, epsilon):
+    iter = 30
+    delta = torch.zeros_like(x)
+    adv_x = x.clone().detach()
+    last_output = None
+    for i in range(iter):
+        iter += 1
+        adv_x.requires_grad = True
+        output = model(adv_x)
+        print('out', output)
+        if i > 0 and not torch.all(torch.eq(last_output.data.sign(), output.data.sign())):
+            print('last', last_output)
+            break
+        last_output = output.clone().detach()
+        model.zero_grad()
+        loss = train_hinge_loss(model, adv_x, y)
+        loss.backward()
+        grad = adv_x.grad.data
+        adv_x = adv_x.detach() + output.detach() * grad/grad.norm(dim=1, p=2)[:, None]
+    print('return', epsilon * delta)
+    return epsilon * delta
+
+def fit(num_epochs, x_train, y_train, x_test, y_test, model, opt, attack, epsilon):
     model.train()
     for epoch in range(num_epochs):
         opt.zero_grad()
-        delta = fgsm(model, x_train, y_train, epsilon)
+        if attack == "fgsm":
+            delta = fgsm(model, x_train, y_train, epsilon)
+        elif attack == "pgd_l2":
+            delta = pgd_l2(model, x_train, y_train, epsilon)
         x_pert = x_train + delta
         y_pred = model(x_pert)
         loss = train_hinge_loss(model, x_pert, y_train)
         # print('loss', loss)
         loss.backward()
         opt.step()
-        
+
     # calc test loss
     model.eval()
     test_loss = test_hinge_loss(model, x_test, y_test)
@@ -78,7 +103,8 @@ def fit(num_epochs, x_train, y_train, x_test, y_test, model, opt, epsilon):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-c', type=float)
+    parser.add_argument('-c', default=0.1, type=float)
+    parser.add_argument('--attack', default="fgsm", type=str, choices=['fgsm', 'pgd_l2'])
     args = parser.parse_args()
     print('args: ', args)
     global c, BEST_MODEL_PATH
@@ -87,7 +113,7 @@ def main():
     BEST_MODEL_PATH = f'kernel_svm_{num_dim}D_{args.c}.pt'
     mu = torch.ones(num_dim)
     sigma = torch.eye(num_dim)
-    epsilons = [0, 0.4, 0.6, 0.8, 1.1, 1.3, 1.5, 2.0, 2.5, 3.0, 5.0, 7.0, 9.0, 12.0, 15.0]
+    epsilons = [0.4, 0.6, 0.8, 1.1, 1.3, 1.5, 2.0, 2.5, 3.0, 5.0, 7.0, 9.0, 12.0, 15.0]
     x_test = torch.cat([torch.distributions.MultivariateNormal(-mu, sigma).sample((TEST_SIZE,)), torch.distributions.MultivariateNormal(mu, sigma).sample((TEST_SIZE,))]).float()	
     y_test = torch.unsqueeze(torch.cat([-torch.ones(TEST_SIZE), torch.ones(TEST_SIZE)]), dim=1).float()	
     test_losses = np.zeros((len(epsilons), TRAIN_SIZE))
@@ -107,7 +133,7 @@ def main():
                 opt = Adam(model.parameters(), lr=learning_rate)
                 # train_set = Data.TensorDataset(x_train, y_train)
                 # train_loader = Data.DataLoader(dataset=train_set, batch_size=train_size, shuffle=True)
-                test_loss = fit(num_epochs, x_train, y_train, x_test, y_test, model, opt, epsilon)
+                test_loss = fit(num_epochs, x_train, y_train, x_test, y_test, model, opt, args.attack, epsilon)
                 temp[j] = test_loss
             mn = np.amin(temp)
             mx = np.amax(temp)
